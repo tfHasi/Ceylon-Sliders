d6bdd0122d6bc01dab636ea29337e2fa88d749fc
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-01-20 01:56:37 +0530

    schema update

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,4 +4,6 @@ class SurfSpot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(100), nullable=False)
+    latitude = db.Column(db.String(100), nullable=False)
+    longitude = db.Column(db.String(100), nullable=False)
     difficulty = db.Column(db.String(50), nullable=False)
