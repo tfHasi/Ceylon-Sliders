0c3fe20fc58f313e4f46990a7b998c1a4d946c97
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-01-20 13:33:21 +0530

    Lat and lon table colomns added

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,7 +3,8 @@ from app import db
 class SurfSpot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(100), nullable=False)
+    direction = db.Column(db.String(100), nullable=False)
     latitude = db.Column(db.String(100), nullable=False)
     longitude = db.Column(db.String(100), nullable=False)
-    difficulty = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+    experience = db.Column(db.String(50), nullable=False)
